71a0a03450269d185cd7fce1b0cdf81efb40cd74
Roman Steger <roman.steger11@gmail.com>
2025-06-20 00:09:11 +0200

    test(blog): testing korrigiert

--- src/app/blog/blog.component.spec.ts
+++ src/app/blog/blog.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { BlogComponent } from './blog.component';
 
 describe('BlogComponent', () => {
@@ -9,6 +10,7 @@ describe('BlogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BlogComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BlogComponent);
