634352a1b4b8341b7f3de2f02e8aaa7c6fe71b34
Roman Steger <roman.steger11@gmail.com>
2025-06-19 22:49:33 +0200

    feat(blog): blog daten laden

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'blog', component: BlogComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+})
+export class AppRoutingModule {}
