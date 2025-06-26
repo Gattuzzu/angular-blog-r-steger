7909d3e845b623e460b4f987bafdadc030fbc296
Roman Steger <roman.steger11@gmail.com>
2025-06-26 23:20:46 +0200

    feat(routes): lazy loading der komponenten "blog/id" und "calculator"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BlogComponent } from './blog/blog.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+// import { CalculatorComponent } from './calculator/calculator.component'; // Wird nur bei Eager Loading benötigt
+// import { BlogDetailComponent } from './blog-detail/blog-detail.component'; // Wird nur bei Eager Loading benötigt
 import { blogDetailResolver } from './service/blog-detail-resolve/blog-detail-resolve.service';
 
 export const routes: Routes = [
-  { path: 'blog', component: BlogComponent },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
   {
     path: 'blog/:id',
-    component: BlogDetailComponent,
+    // component: BlogDetailComponent, // Wird nicht mehr benötig, da dies Eager Loading ist.
     resolve: { data: blogDetailResolver },
+    loadComponent: () =>
+      import('./blog-detail/blog-detail.component').then(
+        (c) => c.BlogDetailComponent,
+      ), // Lazy Loading aktivieren
   },
-  { path: 'calculator', component: CalculatorComponent },
+  {
+    path: 'calculator',
+    // component: CalculatorComponent, // Wird nicht mehr benötig, da dies Eager Loading ist.
+    loadComponent: () =>
+      import('./calculator/calculator.component').then(
+        (c) => c.CalculatorComponent,
+      ), // Lazy Loading aktivieren
+  },
   { path: '', redirectTo: '/blog', pathMatch: 'full' },
 ];
 
