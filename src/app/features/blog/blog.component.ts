2dc6036b2402cde2cb2a0ba6f3165c89c201f462
Roman Steger <roman.steger11@gmail.com>
2025-07-15 20:51:05 +0200

    refactor(blog-card): smart/dump design für blog overview umsetzen

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterModule, RouterLink } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 import { BlogService } from '../../core/service/blog/blog.service';
+import { BlogCardComponent } from '../../shared/blog-card/blog-card.component';
 
 @Component({
   selector: 'app-blog',
-  imports: [RouterModule, RouterLink, MatCardModule],
+  imports: [BlogCardComponent],
   styleUrl: './blog.component.scss',
   standalone: true,
   template: `
@@ -17,34 +17,10 @@ import { BlogService } from '../../core/service/blog/blog.service';
     } @else {
       <div class="blog-container">
         @for (blog of blogs; track blog.id) {
-          <mat-card
-            class="blog-card"
-            appearance="outlined"
-            [routerLink]="['/blog/', blog.id]"
-          >
-            <p>{{ blog.id }}</p>
-            <mat-card-header>
-              <mat-card-title>{{ blog.title }}</mat-card-title>
-              <mat-card-subtitle>{{ blog.author }}</mat-card-subtitle>
-            </mat-card-header>
-            <img
-              mat-card-image
-              [src]="
-                blog.headerImageUrl !== '' &&
-                typeof blog.headerImageUrl === 'string'
-                  ? blog.headerImageUrl
-                  : 'images/pictureNotFound.png'
-              "
-              alt="Missing Picture"
-            />
-            <mat-card-content>
-              <p>{{ blog.contentPreview }}</p>
-            </mat-card-content>
-            <mat-card-actions>
-              <button matButton>LIKE</button>
-              <button matButton>SHARE</button>
-            </mat-card-actions>
-          </mat-card>
+          <app-blog-card
+            [blogEntry]="blog"
+            (blogId)="handleClickOnBlogEntry($event)"
+          ></app-blog-card>
         } @empty {
           <p>Keine Blogeinträge vorhanden.</p>
         }
@@ -56,6 +32,8 @@ export class BlogComponent implements OnInit {
   blogService = inject(BlogService);
   title = 'Blog';
 
+  constructor(private _router: Router) {}
+
   ngOnInit() {
     this.blogService.loadBlogs();
   }
@@ -66,5 +44,9 @@ export class BlogComponent implements OnInit {
 
   get isLoading() {
     return this.blogService.loading();
+  }
+
+  handleClickOnBlogEntry(blogId: number) {
+    this._router.navigate(['/blog/', blogId]);
   }
 }
