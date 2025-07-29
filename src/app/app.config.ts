681b076f0d6731b3364fc9eb617b6658121e5468
Roman Steger <roman.steger11@gmail.com>
2025-07-29 17:55:25 +0200

    refactor(interceptor): interceptor nach core verschoben

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,8 +11,8 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { authInterceptor } from './interceptors/auth.interceptor';
-import { errorInterceptor } from './interceptors/error.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 import { GlobalErrorHandlerService } from './core/errorHandler/global-error-handler.service';
 
