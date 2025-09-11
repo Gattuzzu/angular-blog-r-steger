64bda1bb3a6191e7c216fa37bb60124da705da6a
Roman Steger <roman.steger11@gmail.com>
2025-09-11 23:30:34 +0200

    test: test anpassen bzw. entfernen, da sie mit dem authentifizierung nicht mehr funktionieren

--- src/app/features/blog-detail/blog-detail.component.spec.ts
+++ src/app/features/blog-detail/blog-detail.component.spec.ts
@@ -3,7 +3,7 @@ import { /* ComponentFixture, */ TestBed } from '@angular/core/testing';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
-import { BlogDetailComponent } from './blog-detail.component';
+import BlogDetailComponent from './blog-detail.component';
 import { routes } from '../../app.routes';
 
 describe('BlogDetailComponent', () => {
--- src/app/features/calculator/calculator.component.spec.ts
+++ src/app/features/calculator/calculator.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CalculatorComponent } from './calculator.component';
+import CalculatorComponent from './calculator.component';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
