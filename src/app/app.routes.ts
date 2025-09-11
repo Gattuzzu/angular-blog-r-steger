07fb75ceb6cc5b03cf513c4eb1a6940a87137f21
Roman Steger <roman.steger11@gmail.com>
2025-09-11 22:46:40 +0200

    feat(app.routes): guard funktion hinzugefügt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { inject, NgModule } from '@angular/core';
+import { CanActivateFn, RouterModule, Routes } from '@angular/router';
 
 // import { BlogComponent } from './blog/blog.component'; // Wird nur bei Eager Loading benötigt
 // import { CalculatorComponent } from './calculator/calculator.component'; // Wird nur bei Eager Loading benötigt
 // import { BlogDetailComponent } from './blog-detail/blog-detail.component'; // Wird nur bei Eager Loading benötigt
 import { blogDetailResolver } from './core/service/blog-detail-resolve/blog-detail-resolve.service';
+import { StateHandler } from './core/state-management/appstate.service';
+
+const authGuard: CanActivateFn = async () => {
+  const stateHandler = inject(StateHandler);
+  const roles = await stateHandler.authState().roles();
+  return (
+    stateHandler.authState().isAuthenticated() &&
+    (roles?.includes('user') ? true : (stateHandler.authState().login(), false))
+  );
+};
 
 export const routes: Routes = [
   {
@@ -22,6 +32,7 @@ export const routes: Routes = [
     path: 'add-blog',
     // component: BlogComponent,
     loadComponent: () => import('./features/add-blog/add-blog.component'),
+    canActivate: [authGuard],
   },
   {
     path: 'blog/:id',
@@ -35,10 +46,7 @@ export const routes: Routes = [
   {
     path: 'calculator',
     // component: CalculatorComponent, // Wird nicht mehr benötig, da dies Eager Loading ist.
-    loadComponent: () =>
-      import('./features/calculator/calculator.component').then(
-        (c) => c.CalculatorComponent,
-      ), // Lazy Loading aktivieren
+    loadComponent: () => import('./features/calculator/calculator.component'), // Lazy Loading aktivieren // das .then ist obsolet, wenn man die Klasse als default export macht
   },
 ];
 
