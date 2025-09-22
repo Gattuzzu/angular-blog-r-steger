699d5c0dc4854d67aa5ecfe6a97cbb01b0670a5f
Roman Steger <roman.steger11@gmail.com>
2025-09-22 23:26:41 +0200

    feat(sidebar.component): sidebar durch neue sidebar ersetzt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { NavbarComponent } from './core/navbar/navbar.component';
+import { SidebarComponent } from './core/navbar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, NavbarComponent],
+  imports: [MatSlideToggleModule, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
