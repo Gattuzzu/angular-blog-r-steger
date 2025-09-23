9c630613dfa1f89a08c86280099dfd4736fe0a07
Roman Steger <roman.steger11@gmail.com>
2025-09-23 21:03:34 +0200

    fix(app.component): entfernen von nicht mehr benötigen imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { SidebarComponent } from './core/navbar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatSlideToggleModule, SidebarComponent],
+  imports: [SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   protected title = 'angular-blog-r-steger';
-  public backgroundRed = false;
-  public receivedMessage = '';
-
-  public toggleBackground() {
-    this.backgroundRed = !this.backgroundRed;
-  }
-
-  onMessageReceived(msg: string) {
-    this.receivedMessage = msg;
-  }
 }
